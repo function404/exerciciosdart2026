06781422305b721911415cd5c80473639a8cb933
Lincoln <Lincolnnovais15a@gmail.com>
2026-03-04 22:13:21 -0300

    fix: correção dos exercicios dart async

--- atividade04-03-2026/exercicio07_async.dart
+++ atividade04-03-2026/exercicio07_async.dart
@@ -1,14 +1,5 @@
-void main() {
-  // Exercício 7: Manipulação de Listas
-  List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+import 'dart:async';
 
-  numeros.add(11);
-  numeros.remove(5);
-
-  print('Tamanho da lista: ${numeros.length}');
-  print('Primeiro elemento: ${numeros.first}');
-  print('Último elemento: ${numeros.last}');
-
-  print('Elementos:');
-  numeros.forEach((numero) => print('- $numero'));
+void main() async {
+  
 }
